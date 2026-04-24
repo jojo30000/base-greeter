20e6128e6378258be935b82b38d55a7f626a84a7
jojo30000 <125453529+jojo30000@users.noreply.github.com>
2026-04-24 13:25:15 +0300

    Add Greeter contract with greeting functionality

---
+++ contracts/Greeter.sol
@@ -0,0 +1,19 @@
+```solidity
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract Greeter {
+ string public greeting;
+
+ constructor(string memory _greeting) {
+     greeting = _greeting;
+ }
+
+ function setGreeting(string memory _greeting) public {
+     greeting = _greeting;
+ }
+
+ function getGreeting() public view returns (string memory) {
+     return greeting;
+ }
+}
